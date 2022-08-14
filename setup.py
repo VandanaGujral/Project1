dbd5169e7a961693212d2de536617cf09726d7bd
VandanaGujral <gujralsona0305@gmail.com>
2022-08-14 11:44:43 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 from typing import List
 
 #Declaring variables for setup function
 PROJECT_NAME="housing-predictor"
-VERSION="0.0.1"
+VERSION="0.0.3"
 AUTHOR="vandana gujral"
 DESCRIPTION="This is my first end to end project"
-PACKAGES=["housing"]
 REQUIREMENT_FILE_NAME="requirements.txt"
 
 def get_requirements_list()->List[str]:
@@ -17,13 +16,13 @@ def get_requirements_list()->List[str]:
     of the libraries mentioned in requirements.txt file
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
+        return requirement_file.readlines().remove("-e .")
 
 setup(
 name=PROJECT_NAME,
 version=VERSION,
 author=AUTHOR,
 description=DESCRIPTION,
-packages=PACKAGES, 
+packages=find_packages(), 
 install_requires=get_requirements_list()
 )
